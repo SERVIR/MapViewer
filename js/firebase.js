179ede4f534ccacd9744867bcf3a90b8a3486d86
Billy Ashmall <billy.ashmall@nasa.gov>
2018-08-23 14:10:06 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,10 +2,10 @@
 * firebase setup
 ****************************/
 var config = {
-    apiKey: "AIzaSyBsGVLO3j096w9PAE3AnVeF-MhA40n0mJY",
-    authDomain: "mapviewer-2c8f0.firebaseapp.com",
-    databaseURL: "https://mapviewer-2c8f0.firebaseio.com",
-    projectId: "mapviewer-2c8f0",
-    storageBucket: "mapviewer-2c8f0.appspot.com",
-    messagingSenderId: "796621040202"
+    apiKey: "YourKeyHereY",
+    authDomain: "mapviewer-Yourinfo.firebaseapp.com",
+    databaseURL: "https://mapviewer-Yourinfo.firebaseio.com",
+    projectId: "mapviewer-Yourinfo",
+    storageBucket: "mapviewer-Yourinfo.appspot.com",
+    messagingSenderId: "Yourinfo"
 };
